3c4b1b9b2304f1c93d90778c373be4a4e51d56be
Mishanya <159712377+blazeweaver32@users.noreply.github.com>
2024-06-02 14:29:53 +0300

    added a system for shooting and destroying the tank

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ class Tank:
         self.rect = pygame.Rect(px, py, TILE, TILE)
         self.direct = direct
         self.moveSpeed = 2
+        self.hp = 5
+
+        self.shotTimer = 0
+        self.shotDelay = 60
+        self.bulletSpeed = 5
+        self.bulletDamage = 1
 
         self.keyLEFT = keyList[0]
         self.keyRIGHT = keyList[1]
@@ -42,6 +48,14 @@ class Tank:
             self.rect.y += self.moveSpeed
             self.direct = 2
 
+        if keys[self.keySHOT] and self.shotTimer == 0:
+            dx = DIRECTS[self.direct][0] * self.bulletSpeed
+            dy =  DIRECTS[self.direct][1] * self.bulletSpeed
+            Bullet(self, self.rect.centerx, self.rect.centery, dx, dy, self.bulletDamage)
+
+            self.shotTimer = self.shotDelay
+
+        if self.shotTimer > 0: self.shotTimer -= 1
     def draw(self):
         pygame.draw.rect(window, self.color, self.rect)
 
@@ -50,6 +64,51 @@ class Tank:
         pygame.draw.line(window, 'white', self.rect.center, (x, y), 4)
 
 
+    def damage(self, value):
+        self.hp -= value
+
+        if self.hp <= 0:
+            objects.remove(self)
+            print(f"Танк {self.color} уничтожен")
+class Bullet:
+    def __init__(self, parent, px, py, dx, dy, damage):
+        bullets.append(self)
+
+        self.parent = parent
+        self.px, self.py = px, py
+        self.dx, self.dy = dx, dy
+        self.damage = damage
+
+    def update(self):
+        self.px += self.dx
+        self.py += self.dy
+
+        if self.px < 0 or self.px > WIDTH or self.py < 0 or self.py > HEIGHT:
+            bullets.remove(self)
+        else:
+            for obj in objects:
+                if obj != self.parent and obj.rect.collidepoint(self.px, self.py):
+                    obj.damage(self.damage)
+                    bullets.remove(self)
+                    break
+
+    def draw(self):
+        pygame.draw.circle(window, "yellow", (self.px, self.py), 2)
+
+
+class Block:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+
+bullets = []
 objects = []
 Tank('blue', 100, 275, 0, (
                            pygame.K_a,
@@ -78,10 +137,14 @@ while play:
 
     keys = pygame.key.get_pressed()
 
+    for bullet in bullets: bullet.update()
     for obj in objects: obj.update()
+
 
     window.fill('black')
 
+
+    for bullet in bullets: bullet.draw()
     for obj in objects: obj.draw()
 
     pygame.display.update()
